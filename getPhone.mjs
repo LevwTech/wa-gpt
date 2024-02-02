8c230fa374d09380fe9a9b0478c29049797688db
levwtech <abdelraahmanmostafa@gmail.com>
2024-02-02 21:04:48 +0200

    fix: return whatsapp url directly in /phone

--- getPhone.mjs
+++ getPhone.mjs
@@ -1,8 +1,5 @@
 import { BOT_PHONE_NUMBER, WHATSAPP_URL} from "./helpers/constants.mjs"
 export const getWhatsAppInfo = (payload) => {
     // TODO validate auth 
-    return {
-        phone: BOT_PHONE_NUMBER,
-        whatsAppUrl: WHATSAPP_URL
-    }
+    return WHATSAPP_URL
 }
