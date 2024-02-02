20ddaa52c3e270ca77c00d6bdd8c1f66fbc4cf92
levwtech <abdelraahmanmostafa@gmail.com>
2024-02-02 23:29:13 +0200

    feat: get whatsapp url automatically from start message and bot phone number

--- helpers/constants.mjs
+++ helpers/constants.mjs
@@ -2,7 +2,7 @@ export const START_MESSAGE = "Start using WhatsApp AI Bot";
 export const START_MESSAGE_REPLY = "Hello! I am a WhatsApp AI Bot. I can help you with your queries. Please type your query and I will try to help you.";
 export const BOT_PHONE_NUMBER = "+15551297446";
 export const MESSAGES_DAYS_TILL_EXPIRATION = 2;
-export const WHATSAPP_URL = `https://api.whatsapp.com/send/?phone=${BOT_PHONE_NUMBER}&text=Start%20using%20WhatsApp%20AI%20Bot!`;
+export const WHATSAPP_URL = `https://api.whatsapp.com/send/?phone=${BOT_PHONE_NUMBER}&text=${START_MESSAGE.replace(/ /g, "%20")}`;
 export const OPEN_AI_SYSTEM_MESSAGE = {
     role: "system",
     content: "You are a helpful assistant inside WhatsApp"
--- openaiAPI.mjs
+++ openaiAPI.mjs
@@ -1,14 +1,14 @@
 import { OPEN_AI_SYSTEM_MESSAGE } from "./helpers/constants.mjs";
 
-export const promptGPT = async (messages) =>{
+export const promptGPT = async (messages) => {
     const headers = {
         Authorization: `Bearer ${process.env.OPENAI_KEY}`,
       };
     const response = await axios.post(
         `https://api.openai.com/v1/chat/completions`,
         {
-            messages: [OPEN_AI_SYSTEM_MESSAGE, ...messages],
-            model: "gpt-3.5-turbo",
+          messages: [OPEN_AI_SYSTEM_MESSAGE, ...messages],
+          model: "gpt-3.5-turbo",
         },
         { headers },
     );
