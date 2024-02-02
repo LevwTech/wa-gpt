a6142a07da07a959fedec9c13ead0c9ee8e0c36c
levwtech <abdelraahmanmostafa@gmail.com>
2024-02-02 20:43:23 +0200

    feat: add handling bad requests

--- helpers/utils.mjs
+++ helpers/utils.mjs
@@ -4,6 +4,11 @@ export const getAction = (event) => event.path.split("/")[1];
 
 export const getTTLByDays = (days) => Math.floor(Date.now() / 1000) + 60 * 60 * 24 * days;
 
+export const handleBadRequest = () => ({
+  statusCode: 400,
+  body: "Bad Request",
+});
+
 export const handleMessageReceived = () => ({
   statusCode: 200,
   body: "Message Received",
