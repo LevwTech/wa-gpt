a3bb6a164c4eb8c4bd7227dc68aaab539afb0bb2
levwtech <abdelraahmanmostafa@gmail.com>
2024-02-03 02:03:50 +0200

    feat: change messages days till expiration to 1

--- helpers/constants.mjs
+++ helpers/constants.mjs
@@ -1,5 +1,5 @@
 export const START_MESSAGE = "Start using WhatsApp AI Bot";
 export const START_MESSAGE_REPLY = "Hello! I am a WhatsApp AI Bot. I can help you with your queries. Please type your query and I will try to help you.";
 export const BOT_PHONE_NUMBER = "+15551297446";
-export const MESSAGES_DAYS_TILL_EXPIRATION = 2;
+export const MESSAGES_DAYS_TILL_EXPIRATION = 1;
 export const WHATSAPP_URL = `https://api.whatsapp.com/send/?phone=${BOT_PHONE_NUMBER}&text=${START_MESSAGE.replace(/ /g, "%20")}`;
